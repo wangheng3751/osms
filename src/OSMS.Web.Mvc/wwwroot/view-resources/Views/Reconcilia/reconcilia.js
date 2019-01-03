dbb92b8ff33ed78c0f27398eb3bbc3bbb7902566
wangheng3751 <693184528@qq.com>
2019-01-03 16:02:50 +0800

    bundle

--- src/OSMS.Web.Mvc/wwwroot/view-resources/Views/Reconcilia/reconcilia.js
+++ src/OSMS.Web.Mvc/wwwroot/view-resources/Views/Reconcilia/reconcilia.js
@@ -1,1 +1,36 @@
-﻿
+﻿function onfilechange(){
+    abp.ui.setBusy('#app');
+    var options = {
+      url: "/Reconcilia/Import",
+      success: function (d) {
+          vm.sellOrders=d.result;
+          vm.success=d.success;
+          vm.wrongCount=d.result.filter(function(item){
+             return item.success==false;
+          }).length;
+      },
+      complete:function(){
+          abp.ui.clearBusy('#app');
+      }
+    };
+    $("#file-form").ajaxSubmit(options);
+}
+
+var vm = new Vue({
+  el: '#app',
+  data: {
+     sellOrders:[],
+     success:false,
+     reconcilia:false,
+     wrongCount:0
+  },
+  methods:{
+     reStart:function(){
+        abp.ui.setBusy('#app');
+        setTimeout(function(){
+            vm.reconcilia=true;
+            abp.ui.clearBusy('#app');
+        },2000);
+     }
+  }
+})
